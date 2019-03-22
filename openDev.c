b4326147c4a2c62b185ab406641b193ad4237919
jatin98batra <jatin98batra@gmail.com>
2019-03-22 15:33:21 +0530

    usage of container_of to point the container structure instead of the cdev itself

--- openDev.c
+++ openDev.c
@@ -1,10 +1,23 @@
 #include"headers.h"
 #include"declarations.h"
-int openDev(struct inode * a, struct file * b)
+int openDev(struct inode * inodep, struct file * filp)
 {
-	printk(KERN_INFO "%s:Begin\n",__func__);
 	
-		
+
+	Dev *ldev;  //pointer to the same structure that was allocated by the kmalloc
+	
+	printk(KERN_INFO "%s:Begin\n",__func__);
+	//TODO: i will also try to see if there is anything related in the struct file{} and struct inode{ } like writable pointer etc
+	// we can have accessed the cdev structure using the inode structure but we want actually the cdev structure which is inside the "struct Dev" 	
+	
+	ldev=container_of( inodep->i_cdev , Dev , c_dev );
+
+	//CROSSCHECK:
+#ifdef DEBUG
+	printk(KERN_INFO "ldev = %p",ldev);	
+#endif 
+
+			
 	
 		
 
