7f0a4d680b43f4b1dfc1dac2f5b60e19ed0c6742
jatin98batra <jatin98batra@gmail.com>
2019-03-22 22:44:15 +0530

    <nothing much as well>

--- writeDev.c
+++ writeDev.c
@@ -1,10 +1,10 @@
 #include"headers.h"
 #include"declarations.h"
-ssize_t write (struct file *fl , const char __user *buf, size_t size , loff_t * pointer);
+ssize_t writeDev (struct file *fl , const char __user *buf, size_t size , loff_t * pointer)
 {
 	printk(KERN_INFO "%s:Begin\n",__func__);
  	
-	printk(KERN_INFO "%s:Inode Number: %ld\n",__func__,a->i_ino);
+	//printk(KERN_INFO "%s:Inode Number: %ld\n",__func__,a->i_ino);
 	
 	
 		
