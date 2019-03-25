3c63baa707052889e8020e4221d1b63c6dd3e331
jatin98batra <jatin98batra@gmail.com>
2019-03-26 01:04:01 +0530

    invocation of createScull

--- writeDev.c
+++ writeDev.c
@@ -2,9 +2,28 @@
 #include"declarations.h"
 ssize_t writeDev (struct file *fl , const char __user *buf, size_t size , loff_t * pointer)
 {
+	struct Dev * ldev;
+	struct Qset * start; 
+	size_t actualSize;
+	ldev=(Dev*)fl->private_data;
+
 	printk(KERN_INFO "%s:Begin\n",__func__);
- 	
 	//printk(KERN_INFO "%s:Inode Number: %ld\n",__func__,a->i_ino);
+
+	if(size > ldev->devSize)
+	{
+		actualSize=ldev->devSize;
+	
+	}
+
+	else
+		actualSize=size;
+
+
+//	printk(KERN_INFO "The size we are passing to the createScull is:%ld\n",actualSize);
+	start=createScull(actualSize,ldev);
+	
+
 	
 	
 		
