2d69fc9daef8fbb31fa6a201c12a36fa7206a7c1
jatin98batra <jatin98batra@gmail.com>
2019-03-26 02:49:05 +0530

    read 4 bytes from the register just for testing

--- writeDev.c
+++ writeDev.c
@@ -5,6 +5,8 @@ ssize_t writeDev (struct file *fl , const char __user *buf, size_t size , loff_t
 	struct Dev * ldev;
 	struct Qset * start; 
 	size_t actualSize;
+	char character;
+	int i;
 	ldev=(Dev*)fl->private_data;
 
 	printk(KERN_INFO "%s:Begin\n",__func__);
@@ -22,6 +24,13 @@ ssize_t writeDev (struct file *fl , const char __user *buf, size_t size , loff_t
 
 //	printk(KERN_INFO "The size we are passing to the createScull is:%ld\n",actualSize);
 	start=createScull(actualSize,ldev);
+	
+		
+	for(i=0;i<4;i++)
+	{
+	character=(*(*(((char**)start->data)+(5))+i));
+	printk(KERN_INFO "Character Rec: %c\n",character);
+	}
 	
 
 	
